cff98d8d2885a95445b02d9dddd9e1ab5541a459
adarsh.s.kumar <adarsh.s.kumar@BDC7-L-1241M0H.dir.svc.accenture.com>
2021-08-20 17:10:11 +0530

    commiting first test casevand the driversfolder

--- src/test/java/com/qa/test/TestBase.java
+++ src/test/java/com/qa/test/TestBase.java
@@ -1,5 +1,30 @@
 package com.qa.test;
 
-public class TestBase {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+public class TestBase {
+	
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		
+	}
+	
+	@Test
+	public void Test1() {
+		System.out.println("test1");
+	}
+	
+	@AfterMethod
+	public void tearDowb() {
+		
+	}
+
+	
 }
