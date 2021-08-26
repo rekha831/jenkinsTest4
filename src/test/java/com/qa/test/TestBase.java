6d72b431f289c436c395087ddd498be9c0134dc1
rekha831 <82863914+rekha831@users.noreply.github.com>
2021-08-26 08:23:33 +0530

    new update

--- src/test/java/com/qa/test/TestBase.java
+++ src/test/java/com/qa/test/TestBase.java
@@ -2,6 +2,8 @@ package com.qa.test;
 
 import java.util.concurrent.TimeUnit;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
